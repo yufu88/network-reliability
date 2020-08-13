deb814b27bec300ec0a92cabb9de03944297b913
yufu88 <shellychang0518@gmail.com>
2020-08-13 11:13:56 +0800

    correct ver

--- numpy_ver.py
+++ numpy_ver.py
@@ -40,6 +40,7 @@ def minimal_path(start_node, end_node):
     return(mp)
 
 def feasible_solutions_get(demand, mp_max_capacity, arc_max_capacity):
+    
     feasible_solutions = []
     options = [list(range(c+1)) for c in mp_max_capacity]
 
@@ -56,9 +57,7 @@ def feasible_solutions_get(demand, mp_max_capacity, arc_max_capacity):
         # 3. flow_sum <= arc_max_capacity
         check[2] = (np.sum(flow.reshape(np.size(N,0),1)*N, axis=0) <= arc_max_capacity).all()
 
-
         if(check.all()==True):
-            a=1
             feasible_solutions.append(flow)
     feasible_solutions = np.array(feasible_solutions)
     return(feasible_solutions)
@@ -69,6 +68,16 @@ def current_capacity_get(feasible_solutions, N):
         current_capacity.append(np.sum(f.reshape(np.size(N,0),1)*N, axis=0))
     current_capacity = np.array(current_capacity)
     return(current_capacity)
+
+def d_MP(current_capacity):
+    I=[]
+    for i in range(len(current_capacity)-1):
+        for j in range(i+1,len(current_capacity)):
+            if i not in I:
+                if((current_capacity[i] <= current_capacity[j]).all()):
+                    I.append(j)
+    d_MP = np.delete(current_capacity,I,0)
+    return(d_MP)
 
 # num of nodes
 node_num = 4
@@ -81,9 +90,19 @@ network(2,3)
 network(1,2)
 network(2,1)
 
+arc_capacity = np.array([
+[0.05, 0.1, 0.25,  0.6],
+[0.1,  0.9,  0,    0],
+[0.1,  0.9,  0,    0],
+[0.1 , 0.3,  0.6,  0],
+[0.1,  0.9,  0,    0],
+[0.05, 0.25, 0.7,  0]
+])
+
 # list of arcs
 arc_index = np.argwhere(node==1)
-arc_max_capacity = np.array([2,3,2,3,3,3])
+arc_num = len(arc_index)
+arc_max_capacity = (np.count_nonzero(arc_capacity, axis=1)-1).reshape(1,len(arc_index))
 
 N = minimal_path(0,3)
 
@@ -92,10 +111,34 @@ cap = N*arc_max_capacity
 mp_max_capacity = [np.min(c[np.nonzero(c)]) for c in cap]
 
 # generate feasible solutions
-
-feasible_solutions = feasible_solutions_get(5, mp_max_capacity, arc_max_capacity)
+demand = 3
+feasible_solutions = feasible_solutions_get(demand, mp_max_capacity, arc_max_capacity)
 
 # current capacity
 current_capacity = current_capacity_get(feasible_solutions, N)
-print(feasible_solutions)
-print(current_capacity)
+
+#d_MP
+d_MP = d_MP(current_capacity)
+
+
+prob_n = 0
+prob_total = 1
+
+#print(d_MP[0])
+for i in range(arc_num):
+    print('d_mp:',d_MP[2][i])
+    for k in range(d_MP[2][i],arc_capacity.shape[1]):
+        print(i,k)
+        print('capacity', arc_capacity[i][k])
+
+
+print(arc_capacity[0][3])
+"""for k in range(arc_num):
+    for i in range(d_MP[0][k], len((arc_capacity[0]))):
+        prob_n += arc_capacity[k][i]
+        print(arc_capacity[k][i])
+    print("n")
+    prob_total = prob_total * prob_n
+    prob_n = 0
+    
+"""
